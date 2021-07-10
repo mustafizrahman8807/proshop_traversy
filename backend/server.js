b507d8f0a931040b20352f256af98fd31cc70548
Mustafijur Rahman <mustafizrahman8807@gmail.com>
2021-07-10 07:26:52 +0600

    Third commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 // const express = require('express');
@@ -7,6 +9,7 @@ import products from './data/products.js';
 // const products = require('./data/products.js');
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -26,5 +29,5 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
